02dc0d21181bd873e63e557d10dc82379bf32567
Minahil shafique <minahal.shafique@gmail.com>
2021-03-16 22:55:02 -0400

    mygame

--- script.js
+++ script.js
@@ -11,27 +11,54 @@ const BALL_SIZE = 20;
 
 // Get the computer paddle element
 const computerPaddle = document.querySelector('.computer-paddle');
+const yourPaddle = document.querySelector('.player-paddle');
 
 // Initial computer paddle y-position and y-velocity
 let computerPaddleYPosition = 0;
 let computerPaddleYVelocity = 1;
 
+let yourPaddleYPosition = 0;
+let yourPaddleYVelocity = 1;
+
 // Update the pong world
 function update() {
 
     // Update the computer paddle's position
-    computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
-
+    // computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
+    computerPaddleYPosition = ballYPosition 
     // If the computer paddle goes off the edge of the screen, bring it back
-    computerPaddleYPosition = computerPaddleYPosition % (GAME_AREA_HEIGHT - PADDLE_HEIGHT);
+
+    // computerPaddleYPosition = computerPaddleYPosition % (GAME_AREA_HEIGHT - PADDLE_HEIGHT);
 
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
 }
 
+function moveUp(){
+    yourPaddleYPosition = yourPaddleYPosition - 10
+    yourPaddle.style.top = `${yourPaddleYPosition}px`
+   
+}
+function moveDown(){
+    yourPaddleYPosition = yourPaddleYPosition + 10
+    yourPaddle.style.top = `${yourPaddleYPosition}px`
+    
+}
+
+document.addEventListener('keydown', function (event) {
+    console.log(event.key)
+    if (event.key === 'ArrowUp') {
+        moveUp();   
+    }
+    if (event.code === 'ArrowDown') {
+        moveDown();
+    }
+  });
+
 // Call the update() function everytime the browser is ready to re-render
 function loop() {
     update();
+    BallMove();
     window.requestAnimationFrame(loop);
 }
 window.requestAnimationFrame(loop);
